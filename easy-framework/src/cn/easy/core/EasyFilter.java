a808a66663aa3e06b428a54b2aad9484fccfced2
yycoder <692895299@qq.com>
2014-10-30 10:01:52 +0800

    test

---
+++ easy-framework/src/cn/easy/core/EasyFilter.java
@@ -0,0 +1,26 @@
+package cn.easy.core;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EasyFilter implements Filter {
+
+	public void destroy() {
+		System.out.println("call destroy method");
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		System.out.println("get new request to filter");
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		System.out.println("call init method");
+	}
+
+}
