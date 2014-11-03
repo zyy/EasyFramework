94979c391c9266c03e6cc58898d5e999fe07c256
zyy <692895299@qq.com>
2014-11-03 16:37:42 +0800

    ModelInject 实现

--- src/cn/easy/core/ModelInjector.java
+++ src/cn/easy/core/ModelInjector.java
@@ -15,19 +15,103 @@
  */
 package cn.easy.core;
 
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.servlet.http.HttpServletRequest;
+
+import cn.easy.plugin.activerecord.ActiveRecordException;
+import cn.easy.plugin.activerecord.Model;
+import cn.easy.plugin.activerecord.Table;
+import cn.easy.plugin.activerecord.TableMapping;
+import cn.easy.util.StringUtil;
 
 public final class ModelInjector {
 
-	@SuppressWarnings("unchecked")
-	public static <T> T inject(Class<?> modelClass, HttpServletRequest request, boolean skipConvertError) {
-		return null;
+	public static <T> T inject(Class<?> modelClass, HttpServletRequest request,
+			boolean skipConvertError) {
+		String modelName = modelClass.getSimpleName();
+		return inject(modelClass, StringUtil.firstCharToLowerCase(modelName),
+				request, skipConvertError);
 	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static final <T> T inject(Class<?> modelClass, String modelName, HttpServletRequest request, boolean skipConvertError) {
-		
-		return null;
+
+	@SuppressWarnings("unchecked")
+	public static final <T> T inject(Class<?> modelClass, String modelName,
+			HttpServletRequest request, boolean skipConvertError) {
+		Object model = null;
+		try {
+			model = modelClass.newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		if (model instanceof Model)
+			injectActiveRecordModel((Model) model, modelName, request,
+					skipConvertError);
+		else
+			injectCommonModel(model, modelName, request, modelClass,
+					skipConvertError);
+		return (T) model;
+	}
+
+	private static final void injectCommonModel(Object model, String modelName,
+			HttpServletRequest request, Class<?> modelClass,
+			boolean skipConvertError) {
+		Method[] methods = modelClass.getMethods();
+		for (Method method : methods) {
+			String methodName = method.getName();
+			if (methodName.startsWith("set") == false) // only setter method
+				continue;
+
+			Class<?>[] types = method.getParameterTypes();
+			if (types.length != 1) // only one parameter
+				continue;
+
+			String attrName = methodName.substring(3);
+			String value = request.getParameter(modelName + "."
+					+ StringUtil.firstCharToLowerCase(attrName));
+			if (value != null) {
+				try {
+					method.invoke(model, TypeConverter.convert(types[0], value));
+				} catch (Exception e) {
+					if (skipConvertError == false)
+						throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static final void injectActiveRecordModel(Model model,
+			String modelName, HttpServletRequest request,
+			boolean skipConvertError) {
+		Table table = TableMapping.getInstance().getTable(model.getClass());
+
+		String modelNameAndDot = modelName + ".";
+
+		Map<String, String[]> parasMap = request.getParameterMap();
+		for (Entry<String, String[]> e : parasMap.entrySet()) {
+			String paraKey = e.getKey();
+			if (paraKey.startsWith(modelNameAndDot)) {
+				String paraName = paraKey.substring(modelNameAndDot.length());
+				Class colType = table.getColumnType(paraName);
+				if (colType == null)
+					throw new ActiveRecordException("The model attribute "
+							+ paraKey + " is not exists.");
+				String[] paraValue = e.getValue();
+				try {
+					Object value = paraValue[0] != null ? TypeConverter
+							.convert(colType, paraValue[0]) : null;
+					model.set(paraName, value);
+				} catch (Exception ex) {
+					if (skipConvertError == false)
+						throw new RuntimeException(
+								"Can not convert parameter: " + modelNameAndDot
+										+ paraName, ex);
+				}
+			}
+		}
 	}
 
 }
---
+++ src/cn/easy/plugin/activerecord/ActiveRecordException.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2011-2014, yycoder 692895299@qq.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.easy.plugin.activerecord;
+
+public class ActiveRecordException extends RuntimeException {
+
+	private static final long serialVersionUID = -8664200544983972951L;
+
+	public ActiveRecordException(String message) {
+		super(message);
+	}
+
+	public ActiveRecordException(Throwable cause) {
+		super(cause);
+	}
+
+	public ActiveRecordException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
