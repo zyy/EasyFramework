76a5e4354bbcb9fab72c9c7bfa9b0b3105a4bbb5
zyy <692895299@qq.com>
2014-11-03 13:33:47 +0800

    InterceptorBuilder实现
    Change-Id: I4d720f905fde9553f3c858c98e48479a1ce59ae2
    Signed-off-by: zyy <692895299@qq.com>

--- src/cn/easy/core/InterceptorBuilder.java
+++ src/cn/easy/core/InterceptorBuilder.java
@@ -16,33 +16,110 @@
 package cn.easy.core;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
+import cn.easy.aop.Before;
+import cn.easy.aop.ClearInterceptor;
+import cn.easy.aop.ClearLayer;
 import cn.easy.aop.Interceptor;
 
 public class InterceptorBuilder {
 
+	private final static Interceptor[] NULL_INTERCEPTOR_ARRAY = new Interceptor[0];
+	private Map<Class<Interceptor>, Interceptor> intersMap = new HashMap<Class<Interceptor>, Interceptor>();
+
+	@SuppressWarnings("unchecked")
 	public void addToInterceptorsMap(Interceptor[] defaultInters) {
-		// TODO Auto-generated method stub
-		
+		if (defaultInters == null)
+			return;
+		for (Interceptor interceptor : defaultInters) {
+			intersMap.put((Class<Interceptor>) interceptor.getClass(),
+					interceptor);
+		}
 	}
 
 	public Interceptor[] buildControllerInterceptors(
 			Class<? extends Controller> controllerClass) {
-		// TODO Auto-generated method stub
-		return null;
+		Before before = controllerClass.getAnnotation(Before.class);
+		return before != null ? createInterceptors(before)
+				: NULL_INTERCEPTOR_ARRAY;
 	}
 
 	public Interceptor[] buildMethodInterceptors(Method method) {
-		// TODO Auto-generated method stub
-		return null;
+		Before before = method.getAnnotation(Before.class);
+		return before != null ? createInterceptors(before)
+				: NULL_INTERCEPTOR_ARRAY;
 	}
 
 	public Interceptor[] buildActionInterceptors(Interceptor[] defaultInters,
 			Interceptor[] controllerInters,
 			Class<? extends Controller> controllerClass,
 			Interceptor[] methodInters, Method method) {
-		// TODO Auto-generated method stub
-		return null;
+		ClearLayer controllerClearLayer = getControllerClearType(controllerClass);
+		if (controllerClearLayer != null)
+			defaultInters = NULL_INTERCEPTOR_ARRAY;
+
+		ClearLayer methodClearLayer = getMethodClearType(method);
+		if (methodClearLayer != null) {
+			controllerInters = NULL_INTERCEPTOR_ARRAY;
+			if (methodClearLayer == ClearLayer.ALL)
+				defaultInters = NULL_INTERCEPTOR_ARRAY;
+		}
+
+		int size = defaultInters.length + controllerInters.length
+				+ methodInters.length;
+		if (size == 0)
+			return NULL_INTERCEPTOR_ARRAY;
+
+		Interceptor[] result = new Interceptor[size];
+		int index = 0;
+		for (int i = 0; i < defaultInters.length; i++) {
+			result[index++] = defaultInters[i];
+		}
+		for (int i = 0; i < controllerInters.length; i++) {
+			result[index++] = controllerInters[i];
+		}
+		for (int i = 0; i < methodInters.length; i++) {
+			result[index++] = methodInters[i];
+		}
+
+		return result;
 	}
 
+	private ClearLayer getMethodClearType(Method method) {
+		ClearInterceptor clearInterceptor = method
+				.getAnnotation(ClearInterceptor.class);
+		return clearInterceptor != null ? clearInterceptor.value() : null;
+	}
+
+	private ClearLayer getControllerClearType(
+			Class<? extends Controller> controllerClass) {
+		ClearInterceptor clearInterceptor = controllerClass
+				.getAnnotation(ClearInterceptor.class);
+		return clearInterceptor != null ? clearInterceptor.value() : null;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Interceptor[] createInterceptors(Before before) {
+		Interceptor[] result = null;
+		Class<Interceptor>[] interceptorClasses = (Class<Interceptor>[]) before
+				.value();
+		if (interceptorClasses != null && interceptorClasses.length > 0) {
+			result = new Interceptor[interceptorClasses.length];
+			for (int i = 0; i < result.length; i++) {
+				result[i] = intersMap.get(interceptorClasses[i]);
+				if (result[i] != null)
+					continue;
+
+				try {
+					result[i] = interceptorClasses[i].newInstance();
+					intersMap.put(interceptorClasses[i], result[i]);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return result;
+	}
 }
